bbf53deb7e6bad3352d4d817f401341323ed389d
John P. Neumann <john.p.neumann@gmail.com>
2015-09-03 23:54:28 -0500

    install some stuff by default

---
+++ initial_setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# Installs the basics for us.
+# Author: johnpneumann
+#
+set -e
+
+# Install xcode command line tools
+xcode-select --install
+# Install homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Tap the cask
+brew install caskroom/cask/brew-cask
+# Install python
+brew install python
+# Upgrade pip and setuptools
+pip install --upgrade pip setuptools
+# Install fabric
+pip install fabric
