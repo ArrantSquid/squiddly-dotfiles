aff2f385903616cd7f45a9adedde04585ba49e19
John P. Neumann <john.p.neumann@gmail.com>
2015-10-06 16:20:03 -0500

    made the initial setup better

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #
 # Installs the basics for us.
+# Run this before running the ansible playbook.
 # Author: johnpneumann
 #
 set -e
@@ -8,11 +9,3 @@ set -e
 xcode-select --install
 # Install homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-# Tap the cask
-brew install caskroom/cask/brew-cask
-# Install python
-brew install python
-# Upgrade pip and setuptools
-pip install --upgrade pip setuptools
-# Install virtualenv and ansible
-pip install virtualenv ansible
