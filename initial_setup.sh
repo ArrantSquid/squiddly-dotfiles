75c45a662fc007cf9407f0b25aed0bff7414e64c
John P. Neumann <john.p.neumann@gmail.com>
2015-10-12 17:15:43 -0500

    make things smarter

--- initial_setup.sh
+++ initial_setup.sh
@@ -9,3 +9,13 @@ set -e
 xcode-select --install
 # Install homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Install python
+brew install python
+# Make the foss repos dir
+mkdir -p ~/repos/foss
+# Clone ansible
+git clone git://github.com/ansible/ansible.git --recursive
+# Cd to ansible
+cd ~/repos/foss/ansible
+# Source our ansibile stuff
+source ./hacking/env-setup
